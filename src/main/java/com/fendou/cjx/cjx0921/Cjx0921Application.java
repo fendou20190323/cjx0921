f978aef0f4d4dc7913db3905f221f75a1679269b
f <f>
2020-10-15 11:52:30 +0800

    1

---
+++ src/main/java/com/fendou/cjx/cjx0921/Cjx0921Application.java
@@ -0,0 +1,13 @@
+package com.fendou.cjx.cjx0921;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cjx0921Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cjx0921Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/fendou/cjx/cjx0921/Cjx0921ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fendou.cjx.cjx0921;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cjx0921ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
